51c4d82edd6c5dbdea672df285aea623e6aface4
Jonagarme <garciajonathan268@gmail.com>
2023-02-18 10:25:16 -0500

    primera parte del login

--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -1,27 +1,88 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget{
-  const LoginPage ({Key? Key}) : super(key: Key);
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? Key}) : super(key: Key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Container(
+        height: 50,
+        child: _textDontHaveAccount(),
+      ),
       body: Stack(
+        //POSICIONAR ELEMENTOS UNO DE ENCIMA  DEL OTRO
         children: [
-          _imageCover()
+          _backgroundCover(context),
+          Column(
+            //POSICIONAR ELEMENTOS UNO DE ABAJO DEL OTRO (VERTICAL)
+            children: [
+              _imageCover(),
+              _textAppName(),
+            ],
+          ),
         ],
-      )
-    );
-  }
-
-  Widget _imageCover(){
-    return Container(
-      child: Image.asset(
-        'assets/img/delivery.png',
-        width: 130,
-        height: 130,
       ),
     );
   }
 
+  Widget _backgroundCover(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height * 0.4,
+      color: Colors.amber,
+    );
+  }
+
+  Widget _textAppName() {
+    return Text(
+      'DELIVERY MSQYL',
+      style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        color: Colors.black,
+      ),
+    );
+  }
+
+  Widget _textDontHaveAccount() {
+    return Row(
+      //UBICAR ELEMENTOS UNO A LADO DEL OTRO (HORIZONTAL)
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'No tienes cuenta?',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 17,
+          ),
+        ),
+        SizedBox(
+          width: 7,
+        ),
+        Text(
+          'Registrate Aqui',
+          style: TextStyle(
+            color: Colors.amber,
+            fontWeight: FontWeight.bold,
+            fontSize: 17,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _imageCover() {
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.only(top: 20, bottom: 15),
+        alignment: Alignment.topCenter,
+        child: Image.asset(
+          'assets/img/delivery.png',
+          width: 130,
+          height: 130,
+        ),
+      ),
+    );
+  }
 }
