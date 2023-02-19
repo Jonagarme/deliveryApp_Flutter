41bec3165fda9742cb9e738b81a5494323caf8ab
Jonagarme <garciajonathan268@gmail.com>
2023-02-19 09:59:49 -0500

    usersProviders terminada

--- lib/src/providers/users_providers.dart
+++ lib/src/providers/users_providers.dart
@@ -1,5 +1,16 @@
+import 'package:flutter_app_delivery/src/env/enviroment.dart';
+import 'package:flutter_app_delivery/src/models/user.dart';
 import 'package:get/get.dart';
 
-class UsersProvider extends GetConnect{
-  
+class UsersProvider extends GetConnect {
+  String url = Enviroment.API_URL + 'api/users';
+
+  Future<Response> create(User user) async {
+    Response response = await post(
+      '$url/create',
+      user.toJson(),
+      headers: {'Content-Type': 'application/json'},
+    ); //esperar hasta que el servidor retorno la respuesta
+    return response;
+  }
 }
