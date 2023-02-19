ac39df9a3d3e5e4b8c784c4c82fdc5844b28be5d
Jonagarme <garciajonathan268@gmail.com>
2023-02-19 15:10:44 -0500

    respondeApi para el inicio de session

---
+++ lib/src/models/response_api.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+ResponseApi responseApiFromJson(String str) =>
+    ResponseApi.fromJson(json.decode(str));
+
+String responseApiToJson(ResponseApi data) => json.encode(data.toJson());
+
+class ResponseApi {
+  ResponseApi({
+     this.success,
+     this.message,
+     this.data
+  });
+
+  bool? success;
+  String? message;
+  dynamic data;
+
+  factory ResponseApi.fromJson(Map<String, dynamic> json) => ResponseApi(
+        success: json["success"],
+        message: json["message"],
+        data: json["data"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "success": success,
+        "message": message,
+        "data": data,
+      };
+}
