eb5f118573c98f97702e65bc09740803e10234a0
Jonagarme <garciajonathan268@gmail.com>
2023-02-20 22:19:49 -0500

    enviando formulario con imagen a firebase

--- lib/src/env/enviroment.dart
+++ lib/src/env/enviroment.dart
@@ -1,3 +1,4 @@
 class Enviroment {
   static const String API_URL = "http://192.168.1.9:3000/";
+  static const String API_URL_OLD = "192.168.1.9:3000";
 }
--- lib/src/providers/users_providers.dart
+++ lib/src/providers/users_providers.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+import 'package:path/path.dart';
+import 'dart:io';
 import 'package:flutter_app_delivery/src/env/enviroment.dart';
 import 'package:flutter_app_delivery/src/models/response_api.dart';
 import 'package:flutter_app_delivery/src/models/user.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class UsersProvider extends GetConnect {
   String url = Enviroment.API_URL + 'api/users';
@@ -14,6 +18,39 @@ class UsersProvider extends GetConnect {
     ); //esperar hasta que el servidor retorno la respuesta
     return response;
   }
+
+  Future<Stream> createWithImageUser(User user, File image) async {
+    Uri uri = Uri.http(Enviroment.API_URL_OLD, '/api/users/createWithImage');
+    final request = http.MultipartRequest('POST', uri);
+    request.files.add(
+      http.MultipartFile(
+        'image',
+        http.ByteStream(
+          image.openRead().cast(),
+        ),
+        await image.length(),
+        filename: basename(image.path),
+      ),
+    );
+    request.fields['user'] = json.encode(user);
+    final response = await request.send();
+    return response.stream.transform(utf8.decoder);
+  }
+
+  /* Future<ResponseApi> createUserWithImage(User user, File image) async {
+    FormData form = FormData({
+      'image': MultipartFile(image, filename: basename(image.path)),
+      'user': json.encode(user)
+    });
+    Response response = await post('$url/createWithImage', form);
+
+    if(response.body == null){
+      Get.snackbar('Erro en la peticion', 'No se pudo crear el usuario');
+      return ResponseApi();
+    }
+    ResponseApi responseApi = ResponseApi.fromJson(response.body);
+    return responseApi;
+  }*/
 
   Future<ResponseApi> login(String email, String password) async {
     Response response = await post(
